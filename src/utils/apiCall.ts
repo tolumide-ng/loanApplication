741ab00e541d3253c01e0de9fbc846b7ae25fce2
tolumide-ng <tolumideshopein@gmail.com>
2025-03-01 00:41:27 +0100

    Adds fastify config for backend and reusable apiCall function on the frontend

---
+++ src/utils/apiCall.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export type HttpMethod = 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH';
+
+const API_URL = 'http://localhost:3000';
+
+type Props = {
+  path: string;
+  params?: Record<string, unknown>;
+  method: HttpMethod;
+  signal?: AbortSignal;
+  data?: unknown;
+};
+
+export async function apiCall<T>({
+  path,
+  params,
+  method,
+  signal,
+  data,
+}: Props): Promise<T> {
+  const url = `${API_URL}/${path}`;
+
+  try {
+    const response = await axios({ url, method, params, data, signal });
+    return response.data as unknown as T;
+  } catch (error) {
+    const err = error as Error;
+    throw new Error(err.message);
+  }
+}
