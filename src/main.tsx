616ed4077945ee5854af881c300045af34948ec9
tolumide-ng <tolumideshopein@gmail.com>
2025-03-01 00:36:42 +0100

    Adds custom styling + removes unused styles and code

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import App from './App.tsx';
 import { theme } from './utils/theme.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
