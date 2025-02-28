34b359a58828e198aa0c64762032de71f8eb3179
tolumide-ng <tolumideshopein@gmail.com>
2025-03-01 00:47:59 +0100

    Creates some types for the application

---
+++ src/utils/types.ts
@@ -0,0 +1,39 @@
+export type UserData = {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+  email: string;
+  phone: string;
+  loanAmount: number;
+  upfrontPayment: number;
+  terms: number;
+  monthlySalary: number;
+  additionalIncome: number;
+  mortgage: number;
+  otherCredits: number;
+  showAdditionalInformation: boolean;
+  showMortgage: boolean;
+  showOtherCredits: boolean;
+  confirm: boolean;
+};
+
+export enum StepId {
+  PersonalInformation = 'personalInformation',
+  ContactDetails = 'contactDetails',
+  LoanRequest = 'loanRequest',
+  FinancialInformation = 'financialInformation',
+  Confirm = 'confirm',
+}
+
+export enum Status {
+  Rest = 'rest',
+  Loading = 'loading',
+  Error = 'error',
+  Success = 'success',
+}
+
+export type FormInput<T, K extends keyof T = keyof T> = {
+  name: K;
+  label: string;
+  type: 'text' | 'date' | 'number' | 'email' | 'checkbox';
+};
