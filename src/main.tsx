89fc9660bb96c45ce2bb80df8899915d005c4366
tolumide-ng <tolumideshopein@gmail.com>
2025-03-01 00:33:19 +0100

    Adds chakra theme and wraps entire application in chakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App.tsx';
+import { theme } from './utils/theme.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
-)
+);
