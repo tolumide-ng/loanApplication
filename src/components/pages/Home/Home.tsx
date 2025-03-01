48ce270e2dfc32969a99eec79a7170b249a1ffbd
tolumide-ng <tolumideshopein@gmail.com>
2025-03-01 03:35:20 +0100

    Displays toaster when ApiError occurs

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,15 +1,32 @@
 import { FormProvider } from 'react-hook-form';
-import { Stack, Text } from '@chakra-ui/react';
+import { Stack, Text, ToastId, useToast } from '@chakra-ui/react';
 import { useHome } from './useHome';
 import { StepOverview } from '@/components/organisms/StepOverview/StepOverview';
+import { useEffect, useRef } from 'react';
+import { Status } from '@/utils/types';
 
 export function Home() {
+  const toast = useToast();
+  const toastIdRef = useRef<ToastId | null>(null);
+
   const { methods, formState, onRestart, onClickBack, onSubmit } = useHome();
+
+  useEffect(() => {
+    if (formState.status === Status.Error) {
+      toastIdRef.current = toast({
+        description: 'Api Error: Please try again later',
+        duration: 900,
+        status: 'error',
+        isClosable: true,
+        position: 'top-right',
+      });
+    }
+  }, [toast, formState]);
 
   return (
     <Stack>
       <Text fontSize={'4xl'} fontWeight={'bold'} mb={'6'}>
-        BnB Loan Application
+        Big Loan Application
       </Text>
       <FormProvider {...methods}>
         <StepOverview
